ff2fad2722f760517e93603b81d7b980ae1f3f0b
German Alvarez <germanalvarez15@gmail.com>
2019-04-17 21:16:14 -0300

    AR prototype using Geolocalization based on Haversine formula

---
+++ Assets/gpsTest.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+public class gpsTest: MonoBehaviour
+{
+    public float lat;
+    public float lon;
+
+
+	public Text textoDistancia;
+    IEnumerator coroutine;
+
+    IEnumerator Start()
+    {
+        coroutine = updateGPS();
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        // Start service before querying location
+        Input.location.Start(0.1f,0.1f);
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            print("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            lat = Input.location.lastData.latitude;
+            lon = Input.location.lastData.longitude;
+        	textoDistancia.text = "Lat: " + lat + "      Lon:" + lon;
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            StartCoroutine(coroutine);
+        }
+
+        
+    }
+
+    IEnumerator updateGPS()
+    {
+        float UPDATE_TIME = 3f; //Every  3 seconds
+        WaitForSeconds updateTime = new WaitForSeconds(UPDATE_TIME);
+
+        while (true)
+        {
+            lat = Input.location.lastData.latitude;
+            lon = Input.location.lastData.longitude;
+            textoDistancia.text = "Lat: " + lat + "      Lon:" + lon;
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+
+            yield return updateTime;
+        }
+    }
+
+    void stopGPS()
+    {
+        Input.location.Stop();
+        StopCoroutine(coroutine);
+    }
+
+    void OnDisable()
+    {
+        stopGPS();
+    }
+}
+
+
+    
---
+++ Assets/haversine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class haversine : MonoBehaviour {
+
+	public float latObjeto = -8.72304f;
+    public float lonObjeto = -8.722305f;
+    //public GameObject objeto;
+    //public MeshRenderer objetoMesh;
+    public GameObject medieval;
+    public Text textoHaversine;
+    public gpsTest posicionJugador;
+	// Use this for initialization
+	void Start () {
+		
+	//objetoMesh = GameObject.Find("PuertaCiudadela").GetComponent<MeshRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		operacion();
+	}
+	void operacion(){
+        float R = 6371000; // metres
+        float omega1 = ((posicionJugador.lat/180)*Mathf.PI);
+        float omega2 = ((latObjeto/180)*Mathf.PI);
+        float variacionomega1 = (((latObjeto - posicionJugador.lat)/180)*Mathf.PI);
+        float variacionomega2 = (((lonObjeto - posicionJugador.lon) / 180) * Mathf.PI);
+        float a = Mathf.Sin(variacionomega1/2) * Mathf.Sin(variacionomega1/2) +
+                    Mathf.Cos(omega1) * Mathf.Cos(omega2) *
+                    Mathf.Sin(variacionomega2/2) * Mathf.Sin(variacionomega2/2);
+        float c = 2 * Mathf.Asin(Mathf.Sqrt(a));
+
+        float d = R * c;
+        int dEnInt = Mathf.RoundToInt(d);
+        if (dEnInt > 150){
+         	//objetoMesh.enabled = false;
+         	medieval.SetActive(false);
+
+        }else{
+        	//objetoMesh.enabled = true;
+        	medieval.SetActive(true);
+
+        } 
+
+        textoHaversine.text = dEnInt.ToString();
+
+       
+    }
+}
